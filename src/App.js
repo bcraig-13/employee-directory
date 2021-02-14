8eee82a4dab90bb8a8496862c66c0ae5b255cf6c
Brandon Craig <twilightraven13@yahoo.com>
2021-02-13 23:45:57 -0800

    Changed App.js to Employee.js. Started building component to display employee info.

--- src/App.js
+++
@@ -1,8 +0,0 @@
-//===COMPONENT GOES HERE===
-function App() {
-  return (
-    <h3>= = Employee table goes here = =</h3>
-  );
-}
-
-export default App;
---
+++ src/Employee.js
@@ -0,0 +1,15 @@
+//===COMPONENT GOES HERE===
+function Employee() {
+  return (
+    <div>
+      <img src="" alt=""></img>
+      <p>NAME</p>
+      <p>PHONE</p>
+      <p>EMAIL</p>
+    </div>
+  );
+}
+
+export default Employee;
+
+//img | name | phone | email
