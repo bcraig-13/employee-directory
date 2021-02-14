7c1f6a9b0c193e73b845efcb75de94f987bfba1e
Brandon Craig <twilightraven13@yahoo.com>
2021-02-14 00:17:28 -0800

    Wrote a script tag on index.html outlining how the random user API should work. Needs to be inserted into Employee component.

--- src/Employee.js
+++ src/Employee.js
@@ -1,5 +1,17 @@
 //===COMPONENT GOES HERE===
 function Employee() {
+  fetch("https://randomuser.me/api/")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        const image = myJson.results.picture.thumbnail,
+        const firstName = myJson.results.name.first,
+        const lastName = myJson.results.name.last,
+        const phone = myJson.results.phone,
+        const email = myJson.results.email
+      });
+    
   return (
     <div>
       <img src="" alt=""></img>
@@ -12,4 +24,3 @@ function Employee() {
 
 export default Employee;
 
-//img | name | phone | email
