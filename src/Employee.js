3d023221a9ab105b0f117947830e8be7c072f71a
Brandon Craig <twilightraven13@yahoo.com>
2021-02-14 01:22:05 -0800

    Removed manifest.json. Randomuser API can query specific number of people with certain responses, but need a way to implement response into component.

--- src/Employee.js
+++ src/Employee.js
@@ -1,26 +1,15 @@
-//===COMPONENT GOES HERE===
 function Employee() {
-  fetch("https://randomuser.me/api/")
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (myJson) {
-        const image = myJson.results.picture.thumbnail,
-        const firstName = myJson.results.name.first,
-        const lastName = myJson.results.name.last,
-        const phone = myJson.results.phone,
-        const email = myJson.results.email
-      });
-    
+
   return (
     <div>
-      <img src="" alt=""></img>
-      <p>NAME</p>
-      <p>PHONE</p>
-      <p>EMAIL</p>
+      <img src={`${image}`} alt={`${firstName} ${lastName}`}></img>
+      <p>
+        {firstName} {lastName}
+      </p>
+      <p>{phone}</p>
+      <p>{email}</p>
     </div>
   );
 }
 
 export default Employee;
-
